9fcce4175b50d99180a99e8749735b7414e01a57
TC Chen <zerox12345@hotmail.com>
2022-03-15 20:56:36 +0800

    read.py

--- read.py
+++ read.py
@@ -11,15 +11,18 @@ with open('reviews.txt', 'r') as f:
 print('留言數總共有', len(data), '筆')
 print('留言的平均長度為', t / count)
 
-new = []
-for d in data:
-	if len(d) < 100:
-		new.append(d)
+# new = []
+# for d in data:
+# 	if len(d) < 100:
+# 		new.append(d)
+# print('留言長度小於100共有', len(new),'筆')
+new = [d for d in data if len(d) < 100]
 print('留言長度小於100共有', len(new),'筆')
 
-good = []
-for d in data:
-	if 'good' in d:
-		good.append(d)
+# good = []
+# for d in data:
+# 	if 'good' in d:
+# 		good.append(d)
+# print('留言有good字眼的共有', len(good),'筆')
+good = [d for d in data if 'good' in d]
 print('留言有good字眼的共有', len(good),'筆')
-print(good[0])
