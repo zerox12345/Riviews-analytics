88e59bb108a53247d3b861fc29f92750473de438
TC Chen <zerox12345@hotmail.com>
2022-04-11 22:41:04 +0800

    add word count function

--- read.py
+++ read.py
@@ -26,3 +26,27 @@ print('留言長度小於100共有', len(new),'筆')
 # print('留言有good字眼的共有', len(good),'筆')
 good = [d for d in data if 'good' in d]
 print('留言有good字眼的共有', len(good),'筆')
+
+#文字計數
+wc = {}
+for line in data:
+	words = line.split()
+	for word in words:
+		if word in wc:
+			wc[word] += 1
+		else:
+			wc[word] = 1
+for word in wc:
+	if wc[word] > 1000000:
+		print(word, wc[word])
+
+while True:
+	word = input('what!?\n')
+	if word == 'q':
+		break
+	if word in wc:
+		print(word, wc[word])
+	else:
+		print('Not found in wc')
+
+print('Thanks for applying')
